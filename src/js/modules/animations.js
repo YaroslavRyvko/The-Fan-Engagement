f921288c1f6ceb49efcf2363317feb491b9ca697
YaroslavRyvko <slavik.ruvko@gmail.com>
2023-06-18 22:17:07 +0300

    2 blocks added

--- src/js/modules/animations.js
+++ src/js/modules/animations.js
@@ -1,9 +1,7 @@
 import  gsap  from "gsap";
-import fullpage from "fullpage.js";
 import SplitType from 'split-type'
 
 export function initAnimations() {
-    // Hero Section
 
     let tl1 = gsap.timeline();
     tl1.to(".preloader", { duration: 1, delay: .5, xPercent: 100, })
@@ -13,11 +11,12 @@ export function initAnimations() {
        .from(".book_a_demo", { duration: 2, x: 10, opacity: 0, ease: "expo", }, '<.25')
        .from(".hero_scroll", { duration: 2, y: 10, opacity: 0, ease: "expo", }, '<.25');
 
-
     const fullPageInstance = new fullpage("#fullpage", {
         licenseKey: 'gplv3-license',
-        anchors:['01', '02', '03', '04'],
+        anchors: ['01', '03', '04', '05', '06', '07', '08', '09'],
         scrollOverflow: true,
+        autoScrolling: true,
+        verticalCentered: false,
         menu: '.nav_menu',
         afterLoad: function(anchorLink, index) {
             history.pushState(null, null, " ");
@@ -25,23 +24,67 @@ export function initAnimations() {
         onLeave: function(index, nextIndex, direction) {
             let section =  document.getElementById(nextIndex.item.id);
 
-            if(nextIndex.anchor == '01'){
+            if(nextIndex.anchor == '01'){ // Hero
                 let tl = gsap.timeline();
                 tl.from(".hero_title span", { duration: 2, x: 100, delay: 1, opacity: 0, ease: "expo", stagger: 0.25, }, '<-1');
                 tl.from(".hero_phone", { duration: 3, delay: 1, opacity: 0 }, '<-.5')
             }
-            else if(nextIndex.anchor == '03'){
+            else if(nextIndex.anchor == '03'){ // Get Results
                 let tl = gsap.timeline();
                 tl.from("#getresults h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
                 tl.from("#getresults .aStats", { duration: 1, opacity: 0, scale: 1.5, stagger: 0.25 }, '<-.5');
             } 
-            else if(nextIndex.anchor == '04') {
+            else if(nextIndex.anchor == '04') { // Why SMS
                 let tl = gsap.timeline();
                 tl.from("#whysms h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
                 tl.from("#whysms .aBox", { duration: 1, opacity: 0, scale: 1.5, stagger: 0.25 }, '<-.5');
                 let splitText = new SplitType(".whysms_text_back", {type:"chars"});
                 let chars = splitText.chars;
                 tl.from(chars, {duration: 1, opacity:0, x:-200, rotation:-20,  ease:"back", stagger: 0.1}, "-=2");
+            }
+            else if(nextIndex.anchor == '05') { // Bring the action #1
+                let tl = gsap.timeline();
+                tl.from("#bringtheaction1 h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
+                tl.from("#bringtheaction1 h3", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=2");
+                tl.from("#bringtheaction1 p", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=1.5");
+                tl.from("#bringtheaction1 .overlay_img2", { duration: 2, y: 100, opacity: 0, ease: "back", }, "-=3");
+                let splitText = new SplitType("#bringtheaction1 .movin_text_back", {type:"words,chars"});
+                let chars = splitText.chars;
+                tl.from(chars, {duration: 1, opacity:0, x:-200, rotation:-20,  ease:"back", stagger: 0.1}, "-=6");
+            }
+            else if(nextIndex.anchor == '06') { // Bring the action #2
+                let tl = gsap.timeline();
+                tl.from("#bringtheaction2 h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
+                tl.from("#bringtheaction2 h3", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=2");
+                tl.from("#bringtheaction2 p", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=1.5");
+                tl.from("#bringtheaction2 .overlay_img2", { duration: 2, y: 100, opacity: 0, ease: "back", }, "-=3");
+                let splitText = new SplitType("#bringtheaction2 .movin_text_back", {type:"words,chars"});
+                let chars = splitText.chars;
+                tl.from(chars, {duration: 1, opacity:0, x:-200, rotation:-20,  ease:"back", stagger: 0.1}, "-=6");
+            }
+            else if(nextIndex.anchor == '07') { // Bring the action #3
+                let tl = gsap.timeline();
+                tl.from("#bringtheaction3 h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
+                tl.from("#bringtheaction3 h3", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=2");
+                tl.from("#bringtheaction3 p", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=1.5");
+                tl.from("#bringtheaction3 .overlay_img2", { duration: 2, y: 100, opacity: 0, ease: "back", }, "-=3");
+                let splitText = new SplitType("#bringtheaction3 .movin_text_back", {type:"words,chars"});
+                let chars = splitText.chars;
+                tl.from(chars, {duration: 1, opacity:0, x:-200, rotation:-20,  ease:"back", stagger: 0.1}, "-=6");
+            }
+            else if(nextIndex.anchor == '08') { // Work
+                let tl = gsap.timeline();
+                tl.from("#work h2", { duration: 2, x: 100, delay: .5, opacity: 0, ease: "expo", });
+                tl.from("#work .overlay_img", { duration: 3, opacity: 0 }, '<-.5')
+                tl.from("#work h3", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=2");
+                tl.from("#work p", { duration: 2, x: -100, opacity: 0, ease: "expo", }, "-=1.5");
+                tl.from("#work .btn-blue", { duration: 2, y: -100, opacity: 0, ease: "expo", }, "-=2");
+                let splitText = new SplitType("#work .light_bg_text_top", {type:"words,chars"});
+                let chars  = splitText.chars;
+                tl.from(chars, {duration: 1, opacity:0, x:-200, rotation:-20,  ease:"back", stagger: 0.1}, "-=4");
+                let splitText2 = new SplitType("#work .light_bg_text_bottom", {type:"words,chars"});
+                let chars2 = splitText2.chars;
+                tl.from(chars2, {duration: 1, opacity:0, x:200, rotation:20,  ease:"back", stagger: 0.1}, "-=4");
             }
         }
     });
@@ -100,4 +143,9 @@ export function initAnimations() {
          const delta = max - min;
          return (direction = 1) => (min + delta * Math.random()) * direction;
      }
+
+
+    //  Console Clear
+
+    console.clear();
 }
